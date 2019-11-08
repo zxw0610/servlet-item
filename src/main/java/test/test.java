da01fda3cd08b9ea0437460cd19bbc81d8f1028b
周小伟 <1378723221@qq.com>
2019-11-08 10:29:07 +0800

    在test包下做了修改 1）把test类删除了 2）新建了一个zhou类     里面写了一个main方法

--- src/main/java/test/test.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-public class test {
-	public static void main(String[] args) {
-		System.out.println("我是测试git修改以后上传的");
-	}
-}
---
+++ src/main/java/test/zhou.java
@@ -0,0 +1,7 @@
+package test;
+
+public class zhou {
+	public static void main(String[] args) {
+		System.out.println("我想带你去浪漫的土耳其");
+	}
+}
